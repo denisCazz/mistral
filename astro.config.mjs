159441822388a7e4e5715f2c2054f260ebed5b56
Denis Cazzulo <deniscazzulo@icloud.com>
2025-04-11 15:08:47 +0200

    Add new dependencies and integrate sitemap, compressor, and robots.txt support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import compressor from 'astro-compressor';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap(), compressor(), robotsTxt()],
+  site: 'https://mistral-blond.vercel.app/',
 });
